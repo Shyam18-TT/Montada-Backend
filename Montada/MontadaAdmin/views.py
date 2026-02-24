61e19a6f14471eb11d81cd3ca4af70a5c11480e9
Shyam18-TT <shyam@trusttech.in>
2026-02-24 11:53:27 +0530

    Trader and analyst fetching api updated

--- Montada/MontadaAdmin/views.py
+++ Montada/MontadaAdmin/views.py
@@ -45,7 +45,7 @@ from .serializers import AdminAnalystListSerializer, AdminTraderListSerializer
 
 
 class AdminPageNumberPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 10
     page_size_query_param = "page_size"
     max_page_size = 100
 
@@ -607,6 +607,7 @@ class AdminTraderListView(generics.ListAPIView):
                 .values("trader_id")
                 .annotate(c=Count("id"))
                 .values("c")
+                .order_by()
             )
             qs = qs.annotate(signals_applied_count=Coalesce(Subquery(applied_subq), 0))
 
