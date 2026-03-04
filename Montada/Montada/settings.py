9fbf4e4b3cfee7ec0642bf51f3b3f1aee9002fab
Shyam18-TT <shyam@trusttech.in>
2026-03-04 15:43:23 +0530

    changes updated

--- Montada/Montada/settings.py
+++ Montada/Montada/settings.py
@@ -150,9 +150,7 @@ DATABASES = {
         'PASSWORD': 'Trust_2021',
         'HOST': '213.175.205.19',
         'PORT': '1433',
-        #  'OPTIONS': {
-        #     'driver': 'ODBC Driver 17 for SQL Server',
-        # },
+        
     },
 }
 
