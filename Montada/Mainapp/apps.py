0cd8b02f45b8c50ae368979acfeee06234caed82
Shyam18-TT <shyam@trusttech.in>
2026-04-14 12:45:17 +0530

    changed

--- Montada/Mainapp/apps.py
+++ Montada/Mainapp/apps.py
@@ -5,7 +5,7 @@ class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Mainapp'
 
-    def ready(self):
-        from .db_timing import setup_db_timing_logging
+    # def ready(self):
+    #     from .db_timing import setup_db_timing_logging
 
-        setup_db_timing_logging()
+    #     setup_db_timing_logging()
