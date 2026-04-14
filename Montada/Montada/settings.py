68903e900d79bbc5c6f7d714609931bef02c8579
Shyam18-TT <shyam@trusttech.in>
2026-04-14 12:14:44 +0530

    setting

--- Montada/Montada/settings.py
+++ Montada/Montada/settings.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     # custom apps
-    'Mainapp',
+    'Mainapp.apps.MainappConfig',
     'Subscriptions',
     'Signals',
     'Followers',
