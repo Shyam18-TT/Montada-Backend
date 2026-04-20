d5a1c107ab1a3cc7a22adb41a707ae30a55dfa9c
Shyam18-TT <shyam@trusttech.in>
2026-04-20 14:29:09 +0530

    change updated

--- Montada/News/routing.py
+++ Montada/News/routing.py
@@ -5,4 +5,5 @@ from .consumers import LiveNewsConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/news/live/$", LiveNewsConsumer.as_asgi()),
+    # re_path(r"news/live/$", LiveNewsConsumer.as_asgi()),
 ]
